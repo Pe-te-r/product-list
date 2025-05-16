07538fa615e8c9923ebf750babb64bafd1615ad0
Pe-te-r <mburupeter8526@gmail.com>
2025-05-16 23:28:39 +0300

    added ts

---
+++ src/items.ts
@@ -0,0 +1,63 @@
+export interface item{
+    img: string
+    p: string
+    h4: string
+    h5: number
+}
+
+export const itemArray: item[] = [
+    {
+        img: "./src/assets/images/image-waffle-desktop.jpg",
+        h4: 'Waffle with Berries',
+        h5: 6.50,
+        p: 'Waffle'
+    },
+    {
+        h4: 'Vanilla Bean Crème Brûlée',
+        h5: 7.00,
+        img: './src/assets/images/image-creme-brulee-desktop.jpg',
+        p: 'Crème Brûlée'
+    },
+    {
+        h4: 'Macaron Mix of Five',
+        h5: 8.00,
+        img: './src/assets/images/image-macaron-desktop.jpg',
+        p: 'Macaron'
+    },
+    {
+        h4: 'Classic Tiramisu',
+        h5: 5.50,
+        img: './src/assets/images/image-tiramisu-desktop.jpg',
+        p: 'Tiramisu'
+    },
+    {
+        h4: 'Pistachio Baklava',
+        h5: 4.00,
+        img: './src/assets/images/image-baklava-desktop.jpg',
+        p: 'Baklava'
+    },
+    {
+        h4: 'Lemon Meringue Pie',
+        h5: 5.00,
+        img: './src/assets/images/image-meringue-desktop.jpg',
+        p: 'Pie'
+    },
+    {
+        h4: 'Red Velvet Cake',
+        h5: 4.50,
+        img: './src/assets/images/image-cake-desktop.jpg',
+        p: 'Cake'
+    },
+    {
+        h4: 'Salted Caramel Brownie',
+        h5: 4.50,
+        img: './src/assets/images/image-brownie-desktop.jpg',
+        p: 'Brownie'
+    },
+    {
+        h4: 'Vanilla Panna Cotta',
+        h5: 6.50,
+        img: './src/assets/images/image-panna-cotta-desktop.jpg',
+        p: 'Panna Cotta'
+    }
+]
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,26 @@
-// const app = document.getElementById('app') as HTMLElement
+import { itemArray, type item } from "./items";
 
-// app.innerHTML='<h1>welcome</h1>'
+console.log(itemArray)
+
+const itemArea = document.getElementById('item-area') as HTMLElement
+
+let htmlString = '';
+
+itemArray.forEach((item:item) => {
+    htmlString +=`<div class="meal">
+      <div class="img">
+        <img src=${item.img} alt="">
+      </div>
+      <div class="bottom">
+        <button>Add to Cart</button>
+        <div class="bottom-description">
+          <p>${item.p}</p>
+          <h4>${item.h4}</h4>
+          <h5 class="price">$ ${item.h5}</h5>
+        </div>
+      </div>
+    </div>`
+})
+console.log(itemArea)
+
+itemArea.innerHTML=htmlString
